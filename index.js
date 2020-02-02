5a73ec824d7c6b9c911774922492a33ea58a7982
wmacibnc <wmacibnc@gmail.com>
2020-02-02 08:26:21 -0300

    Ajustando cadastro de produtos

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
-function ListaLivroViewModel(params) {
-    this.parametros = params;
-}
-
-function LivroViewModel(params) {
-    this.parametros = params;
-}
-
-function DetalharLivroViewModel(params) {
-    this.parametros = params;
-}
-
 function IndexViewModel() {
     var self = this;
-
-    self.exibirDetalhar = ko.observable(true);
+    
+    self.produto =  ko.observable({nome:'', preco: 0});
     self.exibirLista = ko.observable(true);
-    self.exibirCadastro = ko.observable(true);
-    self.subtitulo = 'Listagem de livros';
-    self.livro = ko.observable(false);
-
+    self.exibirDetalhar = ko.observable(false);
+    self.exibirCadastro = ko.observable(false);
+    self.id = 5;
+    self.produtos =  ko.observableArray([]);
 
     function init() {
-        self.livros = [
-            { nome: "nome a", autor: "autor a" },
-            { nome: "nome b", autor: "autor b" },
-            { nome: "nome c", autor: "autor c" },
-            { nome: "nome d", autor: "autor d" }
-        ];
+        newProduto({ id: 1, nome: "nome a", preco: 5 })
+        newProduto({ id: 1, nome: "nome b", preco: 15 })
+        newProduto({ id: 1, nome: "nome c", preco: 10 })
+        newProduto({ id: 1, nome: "nome d", preco: 20 })
+
+        console.log(self.produtos())
     };
 
-    self.detalhar = (livro) => {
-        self.livro = (livro);
-        console.log(livro);
+    function newProduto(produto){
+        self.produtos().push(produto);
+    }
+
+    self.detalhar = (produto) => {
+        console.log(produto);
+        self.produto(produto);
         self.exibirDetalhar(true);
+        self.exibirCadastro(true);
         self.exibirLista(false);
     }
 
-    ko.components.register('lista-livro', {
-        template: { element: 'lista-livros-template' },
-        viewModel: ListaLivroViewModel
-    });
+    self.cadastro = function() {        
+        console.log(self.produto());
+        if(self.produto().id == null){
+            self.produto().id = self.id++;
+        }else{
+            remove(self.produtos(), self.produto())
+        }
+        self.produtos.push(self.produto());
+        self.exibirDetalhar(false);
+        self.exibirCadastro(false);
+        self.exibirLista(true);
+    }
 
-    ko.components.register('cadastro-livro', {
-        template: { element: 'livro-template' },
-        viewModel: LivroViewModel
-    });
+    self.novo = function(){
+        self.produto({});
+        self.exibirDetalhar(false);
+        self.exibirCadastro(true);
+        self.exibirLista(false);
 
-    ko.components.register('detalhar-livro', {
-        template: { element: 'detalhar-livro-template' },
-        viewModel: DetalharLivroViewModel
-    });
+    }
+
+    self.excluir = (produto) => {      
+        remove(self.produtos(), produto)
+        self.produto({});
+    }
+
+    self.voltar = () =>{
+        self.exibirDetalhar(false);
+        self.exibirCadastro(false);
+        self.exibirLista(true);
+    }
+    function remove(arr, item) {
+        for (var i = arr.length; i--;) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+            }
+        }
+        self.produtos(arr);
+    }
+
+    self.valorTotal = ko.pureComputed(function() {
+        let valorTotal = 0;
+        self.produtos().forEach(p => {
+            valorTotal += parseInt(p.preco);
+        });
+        return valorTotal;
+    }, this);
 
     init();
 }
